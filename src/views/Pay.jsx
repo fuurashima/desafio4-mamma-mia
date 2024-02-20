2ada7a98cd831885be74662c6666f22c8fec067d
Iván Cano Silva <icano@outlook.cl>
2024-02-19 23:34:00 -0300

    Desafío 4 - Mamma Mia!

---
+++ src/views/Pay.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Pay = ()=> {
+    return(
+        <main className="pagos">
+            <i class="fa-regular fa-credit-card"></i>
+            <h1>Espere un momento por favor, se abrirá el portal de pagos....</h1>
+        </main>
+    )
+}
+
+export default Pay
